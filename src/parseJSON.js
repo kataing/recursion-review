603f687f87ecacc13408307a4a1f83df6ce5883f
kataing <kataing12@gmail.com>
2019-05-28 12:33:32 -0700

    started writing out pseudo code for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,12 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  
+  // 1. Remove outer quotations
+  // 2. Identify first character
+  //  - {} --> obj
+  //  - [] --> array
+  //  - 'null' --> null
+  //  - 0-9 --> loop through until end of number
+  //  - 
 };
