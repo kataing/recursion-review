6ab2b123153682c396349cff11903052d41354b0
kataing <kataing12@gmail.com>
2019-05-28 20:14:59 -0700

    completed getElements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,47 +1,31 @@
 // If life was easy, we could just do things the easy way:
-var getElementsByClassName = function (className) {
-  console.log(document.getElementsByClassName(className));
-  return document.getElementsByClassName(className);
-};
+// var getElementsByClassName = function (className) {
+//   console.log(document.getElementsByClassName(className));
+//   return document.getElementsByClassName(className);
+// };
 
 // But instead we're going to implement it from scratch:
-// var getElementsByClassName = function(className) {
-//   // your code here
-//   var resultsArray = [];
-//   var nodes;
+var getElementsByClassName = function(className) {
+  // your code here
+  var resultsArray = [];
 
-//   // var addElements = function(element) {
-//   //   if (element.classList && element.classList.contains(className)) {
-//   //     resultsArray.push(element);
-//   //   }
-//   //   return resultsArray;
-//   // }
+  var addElements = function(element) {
+    if (element.classList && element.classList.contains(className)) {
+      resultsArray.push(element);
+    }
+    if(element.childNodes) {
+      for(var i = 0; i < element.childNodes.length; i++) {
+        addElements(element.childNodes[i]);
+      }
+    }
+  };
 
-//   // var searchChildNodes = function(parentNode) {
-//   //   for(var i = 0; i < nodes.length, i++) {
-//   //     if(!nodes.childNodes) {
-//   //       addElements(nodes[i]);
-//   //     } else {
-//   //       searchChildNodes(nodes[i]);
-//   //     }
-//   // }
 
-//   // return searchChildNodes(nodes);
 
-//   // var searchChildNodes = function(parentNode) {
-//   //   var childNodes = parentNode.childNodes;
+  
+  
+  
+  addElements(document.body);
 
-//   //   for(var i = 0; i < childNodes.length; i++) {
-//   //     addElements(childNodes[i]);
-//   //   }
-//   // }
-
-//   if(somecriteria) {
-//     nodes = document.getElementsByTagName("*");
-//   } else {
-//     nodes = nodes.childNodes;
-//   }
-
-//  return getElementsByClassName(className);
-
-// };
+  return resultsArray;
+};
